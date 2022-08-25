1ac37246b1d06b2a834031948e58c27872a39c74
潘静怡 <1003346758@qq.com>
2022-08-25 19:10:14 +0800

    feat(main): 组件封装

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,67 @@
+<!--
+ * @Author: Pan Jingyi
+ * @Date: 2022-08-23 01:20:41
+ * @LastEditTime: 2022-08-23 22:08:07
+-->
 <template>
-  <div>1231546</div>
+  <div class="container">
+    <ColumnList :list="list" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from '@/components/ColumnList.vue'
+
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '这是test1专栏',
+    avatar: 'https://picsum.photos/id/858/200/200'
+  },
+  {
+    id: 2,
+    title: 'test1的专栏',
+    description: '这是test1专栏',
+    avatar: 'https://picsum.photos/id/858/200/200'
+  },
+  {
+    id: 2,
+    title: 'test1的专栏',
+    description: '这是test1专栏',
+    avatar: 'https://picsum.photos/id/858/200/200'
+  },
+  {
+    id: 3,
+    title: 'test1的专栏',
+    description: '这是test1专栏',
+    avatar: 'https://picsum.photos/id/858/200/200'
+  },
+  {
+    id: 4,
+    title: 'test1的专栏',
+    description: '这是test1专栏',
+    avatar: 'https://picsum.photos/id/858/200/200'
+  },
+  {
+    id: 5,
+    title: 'test1的专栏',
+    description: '这是test1专栏',
+    avatar: 'https://picsum.photos/id/858/200/200'
+  }
+]
+
 export default defineComponent({
   name: 'App',
+  components: {
+    ColumnList
+  },
   setup() {
-    return {}
+    return {
+      list: testData
+    }
   }
 })
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+/*
+ * @Author: Pan Jingyi
+ * @Date: 2022-08-23 01:20:41
+ * @LastEditTime: 2022-08-23 21:28:42
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path' //这个path用到了上面安装的@types/node
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve('./src') // @代替src
+    }
+  }
 })
